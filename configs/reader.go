3e243f20ee754192bf063f20f4c2a1023be46567
Jian Han <jianhan@mail.com>
2018-01-21 15:54:09 +1000

    initial commit

---
+++ configs/reader.go
@@ -0,0 +1,73 @@
+package configs
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type ConfigsReader interface {
+	Read() (*ServiceConfigs, error)
+}
+
+func NewConfigsReader(env string, extraConfigPaths ...string) ConfigsReader {
+	return &scfgReader{
+		env:              env,
+		extraConfigPaths: extraConfigPaths,
+	}
+}
+
+type scfgReader struct {
+	env              string
+	extraConfigPaths []string
+}
+
+type ServiceConfigs struct {
+	Name             string
+	RegisterTTL      int
+	RegisterInterval int
+	Version          string
+	Metadata         map[string]string
+}
+
+func (s *ServiceConfigs) Validate() error {
+	if strings.TrimSpace(s.Name) == "" {
+		return errors.New("service name can not be empty, please set it in configuration yml file.")
+	}
+	return nil
+}
+
+func (s *scfgReader) Read() (*ServiceConfigs, error) {
+	paths := append([]string{"../configs/base.yml", fmt.Sprintf("../configs/%s.yml", s.env)}, s.extraConfigPaths...)
+	for _, path := range paths {
+		fileInfo, err := os.Stat(path)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"config_file_path": path,
+			}).Warn("file path not exists")
+			return nil, err
+		}
+		fileName, fileExt := fileInfo.Name(), filepath.Ext(fileInfo.Name())
+		fileBaseName := fileName[0 : len(fileName)-len(fileExt)]
+		fileDir := filepath.Dir(path)
+		viper.SetConfigName(fileBaseName)
+		viper.AddConfigPath(fileDir) // path to look for the config file in
+		err = viper.MergeInConfig()  // Find and read the config file
+		if err != nil {              // Handle errors reading the config file
+			log.WithFields(log.Fields{
+				"error": err.Error,
+			}).Warn("failed to read/merge configs")
+			return nil, err
+		}
+	}
+	c := &ServiceConfigs{}
+	if err := viper.UnmarshalKey("service", c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
