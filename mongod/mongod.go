d873a2e38f7e346e54e618d5303162e908230245
Jian Han <jianhan@mail.com>
2018-01-26 12:14:50 +1000

    Added a new struct for generic repo

--- mongod/mongod.go
+++ mongod/mongod.go
@@ -11,3 +11,15 @@ func CreateSession(url string) (*mgo.Session, error) {
 	}
 	return s, nil
 }
+
+type MRepository struct {
+	session *mgo.Session
+}
+
+func (m *MRepository) Close() {
+	m.session.Close()
+}
+
+func (m *MRepository) Collection(dbName, collection string) *mgo.Collection {
+	return m.session.DB(dbName).C(collection)
+}
