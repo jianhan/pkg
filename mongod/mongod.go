cc50f60d61ae2ab8eb5f8b1c7b7e8c8e87f44b0c
Jian Han <jianhan@mail.com>
2018-01-26 10:49:51 +1000

    Remove options

--- mongod/mongod.go
+++ mongod/mongod.go
@@ -4,11 +4,10 @@ import (
 	mgo "gopkg.in/mgo.v2"
 )
 
-func NewSession(url string) (*mgo.Session, error) {
+func CreateSession(url string) (*mgo.Session, error) {
 	s, err := mgo.Dial(url)
 	if err != nil {
 		return nil, err
 	}
-	s.SetMode(mgo.Monotonic, true)
 	return s, nil
 }
