ae2885683da1ff72d021ba2a4b45b30f53d01131
Jian Han <jianhan@mail.com>
2018-01-30 18:59:59 +1000

    [DEV] Added validation for slice of slugs

--- validation/validator.go
+++ validation/validator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/gosimple/slug"
 )
 
 // ValidateSliceUUID performs validation to check if all IDs are valid uuids.
@@ -11,6 +12,16 @@ func ValidateSliceUUID(uuids []string) error {
 	for _, v := range uuids {
 		if !govalidator.IsUUID(v) {
 			return fmt.Errorf("ID: %s is not a valid UUID", v)
+		}
+	}
+	return nil
+}
+
+// ValidateSliceSlugs checks if a slice of slugs are all valid.
+func ValidateSliceSlugs(slugs []string) error {
+	for _, v := range slugs {
+		if !slug.IsSlug(v) {
+			return fmt.Errorf("%s is not a valid slug", v)
 		}
 	}
 	return nil
